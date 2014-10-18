37458f4e22cd3e8c634bdfe156a8f07f55696e39
amwoods3 <amwoods3@cougars.ccis.edu>
2014-10-18 13:26:16 -0500

    Fixed BLACK constant to be black color instead of white.

--- src/constants.h
+++ src/constants.h
@@ -6,7 +6,7 @@
 
 //COLORS
 const SDL_Color WHITE = {255, 255, 255};
-const SDL_Color BLACK = {255, 255, 255};
+const SDL_Color BLACK = {0, 0, 0};
 const SDL_Color GREEN = {0, 255, 0};
 const SDL_Color RED = {255, 0, 0};
 const SDL_Color BLUE = {0, 0, 255};
